e55485e10656af3a1efc4d1d4a8e13c391ce91a6
Mathew Simon <mat-simon@users.noreply.github.com>
2021-11-03 15:38:43 -0700

    slight refactor and tested 4 encrypt
    finished and tested 4 Encrypt functions,
    still needs keyExpand, option for different key sizes, decrypt

--- AES.cpp
+++ AES.cpp
@@ -11,24 +11,25 @@ void MixColumns(vector<vector<unsigned char>> &state);
 void AddRoundKey(vector<vector<unsigned char>> &state, vector<vector<unsigned char>> roundKey);
 void Encrypt(vector<vector<unsigned char>> &state, vector<vector<unsigned char>> roundKey);
 void printSquareMatrix(vector<vector<unsigned char>> state);
+void keyExpand(unsigned char key[16], vector<unsigned int> &w);
 
 const unsigned char SBox[16][16] = {
-    {99, 124, 119, 123, 242, 107, 111, 197, 48, 1, 103, 43, 254, 215, 171, 118},
-    {202, 130, 201, 125, 250, 89, 71, 240, 173, 212, 162, 175, 156, 164, 114, 192},
-    {183, 253, 147, 38, 54, 63, 247, 204, 52, 165, 229, 241, 113, 216, 49, 21},
-    {4, 199, 35, 195, 24, 150, 5, 154, 7, 18, 128, 226, 235, 39, 178, 117},
-    {9, 131, 44, 26, 27, 110, 90, 160, 82, 59, 214, 179, 41, 227, 47, 132},
-    {83, 209, 0, 237, 32, 252, 177, 91, 106, 203, 190, 57, 74, 76, 88, 207},
-    {208, 239, 170, 251, 67, 77, 51, 133, 69, 249, 2, 127, 80, 60, 159, 168},
-    {81, 163, 64, 143, 146, 157, 56, 245, 188, 182, 218, 33, 16, 255, 243, 210},
-    {205, 12, 19, 236, 95, 151, 68, 23, 196, 167, 126, 61, 100, 93, 25, 115},
-    {96, 129, 79, 220, 34, 42, 144, 136, 70, 238, 184, 20, 222, 94, 11, 219},
-    {224, 50, 58, 10, 73, 6, 36, 92, 194, 211, 172, 98, 145, 149, 228, 121},
-    {231, 200, 55, 109, 141, 213, 78, 169, 108, 86, 244, 234, 101, 122, 174, 8},
-    {186, 120, 37, 46, 28, 166, 180, 198, 232, 221, 116, 31, 75, 189, 139, 138},
-    {112, 62, 181, 102, 72, 3, 246, 14, 97, 53, 87, 185, 134, 193, 29, 158},
-    {225, 248, 152, 17, 105, 217, 142, 148, 155, 30, 135, 233, 206, 85, 40, 223},
-    {140, 161, 137, 13, 191, 230, 66, 104, 65, 153, 45, 15, 176, 84, 187, 22},
+    {0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76},
+    {0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0},
+    {0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15},
+    {0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75},
+    {0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84},
+    {0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF},
+    {0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8},
+    {0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2},
+    {0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73},
+    {0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB},
+    {0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79},
+    {0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08},
+    {0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A},
+    {0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E},
+    {0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF},
+    {0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16}
 };
 
 const unsigned char mul2[] = {
@@ -69,17 +70,13 @@ const unsigned char mul3[] = {
     0x0b,0x08,0x0d,0x0e,0x07,0x04,0x01,0x02,0x13,0x10,0x15,0x16,0x1f,0x1c,0x19,0x1a
 };
 
-const unsigned char MIXCOL[4][4] = {
-    {2, 3, 1, 1},
-    {1, 2, 3, 1},
-    {1, 1, 2, 3},
-    {3, 1, 1, 2}
-};
+vector<unsigned int> w[44];
 
 
 int main(){
 
     const string message = "This will be the";
+    const unsigned char key[16] = {0x0F, 0x15, 0x71, 0xC9, 0x47, 0xD9, 0xE8, 0x59, 0x0C, 0xB7, 0xAD, 0xD6, 0xAF, 0x7F, 0x67, 0x98};
     const int bits = 128;
     const int cols = 4;
     const int rows = (bits/8)/4;
@@ -95,6 +92,8 @@ int main(){
             }
         }
     }
+
+    // keyExpand(key, w);
 
     roundKey[0][0] = 172;
     roundKey[0][1] = 25;
@@ -125,7 +124,8 @@ int main(){
 
 
 void Encrypt(vector<vector<unsigned char>> &state, vector<vector<unsigned char>> roundKey){
-    for(int i=0; i<9; i++){                                 //9 rounds for 128 bit AES
+    AddRoundKey(state, roundKey);
+    for(int i=0; i<9; i++){                                 //9 total rounds for 128 bit AES
         SubBytes(state);
         ShiftRows(state);
         MixColumns(state);
@@ -162,14 +162,8 @@ void MixColumns(vector<vector<unsigned char>> &state){
 
     for(int j=0; j<4; j++){
         temp[0][j] = mul2[state[0][j]] ^ mul3[state[1][j]] ^ state[2][j] ^ state[3][j];
-    }
-    for(int j=0; j<4; j++){
         temp[1][j] = state[0][j] ^ mul2[state[1][j]] ^ mul3[state[2][j]] ^ state[3][j];
-    }
-    for(int j=0; j<4; j++){
         temp[2][j] = state[0][j] ^ state[1][j] ^ mul2[state[2][j]] ^ mul3[state[3][j]];
-    }
-    for(int j=0; j<4; j++){
         temp[3][j] = mul3[state[0][j]] ^ state[1][j] ^ state[2][j] ^ mul2[state[3][j]];
     }
 
@@ -194,8 +188,33 @@ void printSquareMatrix(vector<vector<unsigned char>> state){
 
     for(int i=0; i<dim; i++){
         for(int j=0; j<dim; j++){
-            cout << hex << static_cast<unsigned>(state[i][j]) << ' ';
+            cout << hex << (unsigned)(state[i][j]) << ' ';
+            //cout << state[i][j];
         }
         cout << endl;
     }
 }
+
+// unsinged int SubWord(unsigned int temp){
+
+// }
+
+// unsigned int RotWord(unsigned int temp){
+
+// }
+
+// void keyExpand(unsigned char key[], vector<unsigned int> &w){
+//     // from page 173 of textbook
+//     unsigned char temp[4];
+//     for(int i = 0; i < 4; i++){
+//         w[i] = key[4*i + i];
+//         cout << w[i] << endl;
+//     }
+//     for(int i = 4; i < 44; i++){
+//         temp = w[i - 1];
+//         if(i % 4 == 0){
+//             // temp = SubWord(RotWord(temp));
+//         }
+//         w[i] = w[i - 4] ^ temp;
+//     }
+// }
